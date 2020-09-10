f1b24ccb892c4147ff30930acc12b9b05b1cab21
Casey B <caseyoksana@gmail.com>
2020-09-10 02:22:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,9 +9,9 @@ new_hash = {}
 emoticons.each do |key, value|
   new_hash[key] = {}
   new_hash[key][:english] = value[0]
-    new_hash[key][:japanese] = value[1]
+  new_hash[key][:japanese] = value[1]
   end
-
+end
   new_hash
 end
 
